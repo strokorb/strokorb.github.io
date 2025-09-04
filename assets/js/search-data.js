ab0572bd523fe2565c64c64a722b689ff6e3eaf6
strokorb <kirstinstrokorb@gmail.com>
2025-09-04 19:50:21 +0000

    Deploying to gh-pages from @ strokorb/strokorb.github.io@6c83f6b962fbf473cc86f6c233125b0addcfac62 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,29 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "news-rss-discussion-on-12-february-2020-the-royal-statistical-society-hosted-a-meeting-to-discuss-the-forth-coming-paper-graphical-models-for-extremes-by-sebastian-engelke-and-adrien-hitz-here-is-the-link-to-the-slides-and-the-extended-version-of-my-discussion-contribution-including-the-proofs",
+        },{id: "news-rss-discussion-on-12-february-2020-the-royal-statistical-society-hosted-a-meeting-to-discuss-the-forth-coming-paper-graphical-models-for-extremes-by-sebastian-engelke-and-adrien-hitz-here-is-the-link-to-the-slides-and-the-extended-version-of-my-discussion-contribution-including-the-proofs",
           title: 'RSS discussion: On 12 February 2020 the Royal Statistical Society hosted a meeting...',
           description: "",
           section: "News",},{id: "news-congratulations-to-jonas-brehmer-on-his-successful-phd-viva-jonas-work-on-theory-and-methodology-of-scoring-functions-tail-properties-interval-forecasts-and-point-processes-has-led-to-several-publications-including-why-scoring-rules-cannot-assess-tail-properties-ejs-2019-scoring-interval-forecasts-equal-tailed-shortest-and-model-interval-bernoulli-2021-and-comparative-evaluation-of-point-process-forecasts-aism-2024",
